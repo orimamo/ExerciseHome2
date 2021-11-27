fb141df9a772bf124259a9cc87fc5d4093567c0c
ori mamo <mori8056@gmail.com>
2021-11-27 18:12:09 +0200

    update

--- src/ExersiceHome7.java
+++ src/ExersiceHome7.java
@@ -5,6 +5,7 @@ public class ExersiceHome7 {
         public static final int HELP_PRINT_BOARD=3;
         public static char[]  gameBoard = new char[ARRYA_SIZE];
         public static void main (String[]args){
+
             getPostionFromUse();
         }
         public static void printBoard (char[] gameBoard){
