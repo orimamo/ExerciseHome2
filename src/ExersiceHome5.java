4e93cfe8eaea0839e9433810956033a61c621c59
ori mamo <mori8056@gmail.com>
2021-11-29 21:44:38 +0200

    update

--- src/ExersiceHome5.java
+++ src/ExersiceHome5.java
@@ -7,44 +7,41 @@ public class ExersiceHome5 {
             top=-1;
         }
 
-        while ((arr[i] < arr[i + 1]) ) {
+        while (arr[i] < arr[i + 1] && i < arr.length)
+        {
             i++;
             top++;
-            if (i == arr.length) {
+            if (i +1 == arr.length) {
                 top = -1;
                 break;
             }
-        }
-        if(arr[i] == arr[i+1])
-        {
-            top = -1;
-        }
-        for (int j = i; j < arr.length; j++)
-        {
-            if (j+1 < arr.length)
-            {
 
-                if (arr[j] > arr[j + 1])
-                {
-                    counter++;
-                } else if (arr[j] < arr[j + 1])
-                {
-                    top = -1;
-                } else
-                {
-                    if (arr.length == top + counter)
-                    {
-                        break;
+        }
+        if (top !=-1) {
+            if (arr[i] == arr[i + 1]) {
+                top = -1;
+            }
+            for (int j = i; j < arr.length; j++) {
+                if (j + 1 < arr.length) {
+
+                    if (arr[j] > arr[j + 1]) {
+                        counter++;
+                    } else if (arr[j] < arr[j + 1]) {
+                        top = -1;
+                    } else {
+                        if (arr.length == top + counter) {
+                            break;
+                        }
                     }
                 }
-            }
 
+            }
         }
             return top;
 
         }
         public static void main (String[]args){
-            int[] array = {0,1,7,9,9,2};
+            int[] array = {0,1,2,4,3,2};
             int ans = upAndDown(array);
             System.out.println(ans);
         }
